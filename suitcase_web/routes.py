ee7a48f8e0f0746688b8746f8e60a35c8ce2d0e8
JEWeyre <laurafarvour@gmail.com>
2019-10-26 22:00:21 -0500

    following flask tutorial

--- suitcase_web/routes.py
+++ suitcase_web/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from suitcase_web import suitcase_app
 
 @suitcase_app.route('/')
 @suitcase_app.route('/index')
 def index():
-	return "Hello, World!"
+	def index():
+		user = {'username': 'Miguel'}
+		return render_template('index.html', title='Home', user=user)
