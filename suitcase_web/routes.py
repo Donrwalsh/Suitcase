4845ff2c409ad5ca83d2a0a44cd6dc6ce5214d2a
JEWeyre <laurafarvour@gmail.com>
2019-10-27 13:42:16 -0500

    Revert "Should accept input to DB and redirect to blank page"
    This reverts commit 611c4644154e91177529d8c19bbbf8846969c170.

--- suitcase_web/routes.py
+++ suitcase_web/routes.py
@@ -1,10 +1,7 @@
-from flask import render_template, url_for
-from werkzeug.utils import redirect
+from flask import render_template
 from suitcase_web import suitcase_app, db
 from suitcase_web.forms import word_input
 from suitcase_web.models import Word, Definitions
-from flask import request
-from werkzeug.urls import url_parse
 
 
 @suitcase_app.route('/')
@@ -17,5 +14,5 @@ def two_words():
 		db.session.add(word1)
 		db.session.add(word2)
 		db.session.commit()
-		return redirect(url_for('/two_words'))
+		return render_template('two_words.html', title='Suitcase', form=form)
 	return render_template('two_words.html', title='Suitcase', form=form)
