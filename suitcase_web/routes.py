320c48c6353d41830dbd5173c35abd251c0aed95
JEWeyre <laurafarvour@gmail.com>
2019-10-26 23:26:01 -0500

    Simplifying out template stuff

--- suitcase_web/routes.py
+++ suitcase_web/routes.py
@@ -3,21 +3,6 @@ from suitcase_web import suitcase_app
 from suitcase_web.word_input import word_input
 
 @suitcase_app.route('/')
-@suitcase_app.route('/index')
-def index():
-	user = {'username': 'Miguel'}
-	posts = [
-		{
-			'author': {'username': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
-
 @suitcase_app.route('/two_words')
 def two_words():
 	form = word_input()
