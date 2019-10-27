611c4644154e91177529d8c19bbbf8846969c170
JEWeyre <laurafarvour@gmail.com>
2019-10-27 13:42:16 -0500

    Should accept input to DB and redirect to blank page

--- suitcase_web/routes.py
+++ suitcase_web/routes.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template, url_for
+from werkzeug.utils import redirect
 from suitcase_web import suitcase_app, db
 from suitcase_web.forms import word_input
 from suitcase_web.models import Word, Definitions
+from flask import request
+from werkzeug.urls import url_parse
 
 
 @suitcase_app.route('/')
@@ -14,5 +17,5 @@ def two_words():
 		db.session.add(word1)
 		db.session.add(word2)
 		db.session.commit()
-		return render_template('two_words.html', title='Suitcase', form=form)
+		return redirect(url_for('/two_words'))
 	return render_template('two_words.html', title='Suitcase', form=form)
