bb6fd3118c35abf91a4527749b889b9ca3347ebe
JEWeyre <laurafarvour@gmail.com>
2019-10-27 12:49:23 -0500

    Fixing app reference for context processor

--- suitcase_app.py
+++ suitcase_app.py
@@ -1,6 +1,6 @@
 from suitcase_web import suitcase_app, db
 from suitcase_web.models import Word, Definitions
 
-@app.shell_context_processor
+@suitcase_app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Word': Word, 'Definition': Definitions}
